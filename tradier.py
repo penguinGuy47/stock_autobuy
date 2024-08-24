873c2e18b2b8b9fb8a4433d0d8cc913fa0213025
Kaleb <kaleblianggg@icloud.com>
2024-08-24 14:16:17 -0500

    added files for brokers, no functionality yet

--- tradier.py
+++ tradier.py
@@ -8,17 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-
 import time
-import random
-
-def short_sleep():
-    random_number = random.randint(1,3)
-    time.sleep(random_number)
-
-def rand_sleep():
-    random_number = random.randint(5, 10)
-    time.sleep(random_number)
+import sleep
 
 # ticker = input("Please enter the ticker you are buying: ")
 ticker = 'GME'
@@ -42,47 +33,47 @@ for char in pw:
     pw_field.send_keys(char)
     time.sleep(0.1) 
 
-time.sleep(2)
+sleep.short_sleep()
 
 sign_in_button = driver.find_element(By.XPATH, "//button[contains(text(),'Sign In')]")
 sign_in_button.click()
 
-WebDriverWait(driver, 300).until(EC.url_changes("https://dash.tradier.com/dashboard"))
+WebDriverWait(driver, 60).until(EC.url_changes("https://dash.tradier.com/dashboard"))
 print("Logged into Tradier!")
 
-rand_sleep()
+sleep.rand_sleep()
 
 ticker_search = driver.find_element(By.NAME, "quote_module_symbol_search")
 for char in ticker:
     ticker_search.send_keys(char)
     time.sleep(0.1) 
 
-short_sleep()
+sleep.short_sleep()
 
 ticker_search.send_keys(Keys.ENTER)
 
-short_sleep()
+sleep.short_sleep()
 
 trade_button = driver.find_element(By.XPATH, "//button[contains(@class, 'button') and contains(@class, 'success') and contains(@class, 'lg')]")
 trade_button.click()
 
-short_sleep()
+sleep.short_sleep()
 
-buy_dropdown = WebDriverWait(driver, 10).until(
+buy_dropdown = wait.until(
     EC.visibility_of_element_located((By.NAME, 'side'))
 )
 buy_dropdown.click()
 select = Select(buy_dropdown)
 select.select_by_visible_text("Buy")
 
-short_sleep()
+sleep.short_sleep()
 
 # quantity of shares to purchase
-quantity_field = WebDriverWait(driver, 10).until(
+quantity_field = wait.until(
     EC.visibility_of_element_located((By.NAME, "quantity"))
 )
 quantity_field.send_keys("1")
 
-rand_sleep()
+sleep.rand_sleep()
 
 driver.quit()
