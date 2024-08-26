bc2626c000ff3b29f7833a0ec5480bd7f0be7530
Kaleb <kaleblianggg@icloud.com>
2024-08-26 13:00:00 -0500

    added ms sleep time method for human-like typing

--- sleep.py
+++ sleep.py
@@ -1,10 +1,14 @@
 import time
 import random
 
+def human_like():
+    random_number = random.uniform(0.1,0.3)
+    time.sleep(random_number)
+
 def short_sleep():
-    random_number = random.randint(1,3)
+    random_number = random.randint(3,4)
     time.sleep(random_number)
 
 def rand_sleep():
-    random_number = random.randint(5, 10)
+    random_number = random.randint(6, 10)
     time.sleep(random_number)
