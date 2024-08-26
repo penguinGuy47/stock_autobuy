032ebca2d7f1e0792d82c8db27abe3c154c856cc
Kaleb <kaleblianggg@icloud.com>
2024-08-26 13:01:34 -0500

    adjusted human-like typing intervals

--- sleep.py
+++ sleep.py
@@ -2,7 +2,7 @@ import time
 import random
 
 def human_like():
-    random_number = random.uniform(0.1,0.3)
+    random_number = random.uniform(0.05,0.25)
     time.sleep(random_number)
 
 def short_sleep():
