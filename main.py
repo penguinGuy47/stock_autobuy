dcf201f987a2f1fa99d52ecff220ad45401656d1
Kaleb <kaleblianggg@icloud.com>
2024-08-27 14:19:47 -0500

    added broker class functions and ticker input

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ import tornado
 import tradier
 import webull
 import wellsfargo
+
+# ticker = input("Enter the ticker you would to purchase: ")
+ticker = 'XCUR'
+
+chase.buy(ticker)
+# tradier.buy(ticker)
