51faccd69d9c542e2aa2dab082712f3f28e8455e
Kaleb <kaleblianggg@icloud.com>
2024-08-27 12:08:48 -0500

    added long sleep method and supporting comments

--- sleep.py
+++ sleep.py
@@ -1,19 +1,22 @@
 import time
 import random
 
+# for typing
 def human_like():
     random_num = random.uniform(0.05,0.25)
     time.sleep(random_num)
 
+# for short human-like waits
 def short_sleep():
     random_num = random.randint(3,4)
     time.sleep(random_num)
 
-# MTF
+# for medium time waiting
 def rand_sleep():
     random_num = random.randint(6, 9)
     time.sleep(random_num)
 
+# for longer waits
 def long_sleep():
     random_num = random.randint(10,15)
     time.sleep(random_num)
