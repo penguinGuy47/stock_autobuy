6474ead658e0f3cf4da9e2ea65a947bbe551806c
Kaleb <kaleblianggg@icloud.com>
2024-08-28 14:54:52 -0500

    added all imports here so other files do not have to repeatedly import

--- sleep.py
+++ sleep.py
@@ -1,5 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
+
 import time
 import random
+import os
 
 # for typing
 def human_like():
