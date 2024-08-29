4a040ac438ad7dcd453c53ec3a50facb522ef6f7
Kaleb <kaleblianggg@icloud.com>
2024-08-29 14:29:33 -0500

    updated sleep calls

--- fidelity.py
+++ fidelity.py
@@ -1,7 +1,5 @@
 from sleep import *
 
-import sleep
-
 # TODO:
 # add multi buy function
 def buy(ticker):
@@ -13,7 +11,7 @@ def buy(ticker):
     # driver.get("https://digital.fidelity.com/prgw/digital/login/full-page")
     driver.get("https://digital.fidelity.com/ftgw/digital/portfolio/summary")
     wait = WebDriverWait(driver, 10)
-    sleep.short_sleep()
+    short_sleep()
 
     # ENTER YOUR CREDENTIALS
     username = ""   # ENTER YOUR USERNAME
@@ -23,15 +21,15 @@ def buy(ticker):
     username_field.click()
     for char in username:
         username_field.send_keys(char)
-        sleep.human_like()
+        human_like()
 
-    sleep.short_sleep()
+    short_sleep()
 
     pw_field = driver.find_element(By.XPATH, '//*[@id="dom-pswd-input"]')
     for char in pw:
         pw_field.send_keys(char)
-        sleep.human_like()
-    sleep.short_sleep()
+        human_like()
+    short_sleep()
 
     log_in_button = driver.find_element(By.XPATH, '//*[@id="dom-login-button"]')
     log_in_button.click()
@@ -44,19 +42,19 @@ def buy(ticker):
     trade_button = driver.find_element(By.XPATH, '//*[@id="action-bar--container"]/div[2]/div[1]/ul/li[1]/a')
     trade_button.click()
 
-    sleep.short_sleep()
+    short_sleep()
 
     # click dropdown
     account_dropdown = driver.find_element(By.XPATH, '//*[@id="dest-acct-dropdown"]')
     account_dropdown.click()
 
-    sleep.very_short_sleep()
+    very_short_sleep()
     print("clicked dropdown")
 
     account_select = driver.find_element(By.XPATH, '//*[@id="ett-acct-sel-list"]')
-    sleep.very_short_sleep()
+    very_short_sleep()
 
-    sleep.short_sleep()
+    short_sleep()
 
     ul_element = driver.find_element(By.XPATH, '//*[@id="ett-acct-sel-list"]/ul')
     li_elements = ul_element.find_elements(By.TAG_NAME, 'li')
@@ -69,40 +67,40 @@ def buy(ticker):
         else:
             account_dropdown = driver.find_element(By.XPATH, '//*[@id="dest-acct-dropdown"]')
             account_dropdown.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
         account_select = driver.find_element(By.XPATH, f'//*[@id="account{num}"]')
         account_select.click()
-        sleep.short_sleep()
+        short_sleep()
 
         symbol_search = driver.find_element(By.XPATH, '//*[@id="eq-ticket-dest-symbol"]')
 
         # enter ticker
         for char in ticker:
             symbol_search.send_keys(char)
-            sleep.human_like()
-        sleep.very_short_sleep()
+            human_like()
+        very_short_sleep()
         symbol_search.send_keys(Keys.ENTER)
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # click buy
         buy_button = driver.find_element(By.XPATH, '//*[@id="action-buy"]/s-root/div')
         buy_button.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # enter quantity
         qty = driver.find_element(By.XPATH, '//*[@id="eqt-shared-quantity"]')
         qty.send_keys("1")
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # click somewhere else
         price_area = driver.find_element(By.XPATH, '//*[@id="quote-panel"]/div/div[2]')
         price_area.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # click limit buy
         limit_buy_button = driver.find_element(By.XPATH, '//*[@id="market-no"]/s-root/div/label')
         limit_buy_button.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # get current price
         current_price = driver.execute_script('return document.querySelector("#eq-ticket__last-price .last-price").textContent')
@@ -114,23 +112,23 @@ def buy(ticker):
         limit_price = driver.find_element(By.XPATH, '//*[@id="eqt-ordsel-limit-price-field"]')
         for char in current_price:
             limit_price.send_keys(char)
-            sleep.human_like()
-        sleep.very_short_sleep()
+            human_like()
+        very_short_sleep()
 
         # preview button
         preview_button = driver.find_element(By.XPATH, '//*[@id="previewOrderBtn"]/s-root/button')
         preview_button.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # submit
         submit_button = driver.find_element(By.XPATH, '//*[@id="placeOrderBtn"]')
         submit_button.click()
-        sleep.very_short_sleep()
+        very_short_sleep()
 
         # enter new order
         new_order_button = driver.find_element(By.XPATH, '//*[@id="eq-ticket__enter-new-order"]')
         new_order_button.click()
-        sleep.short_sleep()
+        short_sleep()
         
     print("No more accounts to process.")
     driver.quit()
