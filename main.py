37153c46eaf6d6331a16b129f646476c64f79152
Kaleb <kaleblianggg@icloud.com>
2024-08-29 17:56:10 -0500

    Remove pycache files and add to .gitignore

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ import wellsfargo
 def main():
     # ticker = input("Enter the ticker you would to purchase: ")
     ticker = 'AMC'
+    chrome_path = r"C:\Users\kaile\AppData\Local\Google\Chrome\User Data" # add the path to your chrome
+    chrome_profile = r"Profile 6" # change this accordingly
 
     # chase.buy(ticker)
     # tradier.buy(ticker)
-    fidelity.buy(ticker)
+    # fidelity.buy(ticker)
+    firstrade.buy(ticker, chrome_path, chrome_profile)
 
 if __name__ == "__main__":
     main()
