2ad2d1176503b479dbbf88ebf2c4c1e41cd127b9
Kaleb <kaleblianggg@icloud.com>
2024-09-07 21:15:44 -0500

    added headless and regular driver starters, save/load cookie functions

--- sleep.py
+++ sleep.py
@@ -11,7 +11,15 @@ from selenium.common.exceptions import TimeoutException
 
 import time
 import random
+import pickle
 import os
+import re
+
+def restart_driver(driver):
+    driver.quit()
+    short_sleep()
+    
+    return start_headless_driver()
 
 def human_type(word, destination):
     random_num = random.uniform(0.05,0.25)
@@ -43,3 +51,49 @@ def rand_sleep():
 def long_sleep():
     random_num = random.randint(10,15)
     time.sleep(random_num)
+
+def start_headless_driver(dir, prof):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Headless mode
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument(f"user-data-dir={dir}")  # COMMENTED FOR 2FA TESTING
+    # options.add_argument(f"--profile-directory={prof}")
+    options.add_argument("--disable-blink-features=AutomationControlled")  # Prevent detection
+    options.add_argument("--window-size=1920,1080")  # standard desktop size window
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-browser-side-navigation")
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+    
+    driver = webdriver.Chrome(options=options, service=Service("chromedriver.exe"))
+    return driver
+
+def start_regular_driver(dir, prof):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument(f"user-data-dir={dir}")  # COMMENTED FOR 2FA TESTING
+    # options.add_argument(f"--profile-directory={prof}")
+    options.add_argument("--disable-blink-features=AutomationControlled")  # prevent detection
+    options.add_argument("--window-size=1920,1080")  # standard desktop size window
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-browser-side-navigation")
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+    
+    driver = webdriver.Chrome(options=options, service=Service("chromedriver.exe"))
+    return driver
+
+def save_cookies(driver, path):
+    with open(path, "wb") as f:
+        pickle.dump(driver.get_cookies(), f)
+
+def load_cookies(driver, path):
+    with open(path, "rb") as f:
+        cookies = pickle.load(f)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        
