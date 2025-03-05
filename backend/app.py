8806468bf1b4ecf7b8f72a217beb4a427875ab76
Kaleb <kaleblianggg@icloud.com>
2025-03-05 11:49:23 -0600

    removed bp for product page

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from controllers.trade_controller import trade_bp
-from controllers.product_controller import product_bp
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 app.register_blueprint(trade_bp, url_prefix='/')
-app.register_blueprint(product_bp, url_prefix='/')
 
 if __name__ == '__main__':
     app.run(debug=True)
