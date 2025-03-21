66db48ea1a7ae178f7ac7aa624324c68a45b6dec
penguinGuy47 <kaleblianggg@icloud.com>
2025-03-21 13:20:33 -0500

    headless mode and removed comments from submit_order()

--- backend/services/robinhood.py
+++ backend/services/robinhood.py
@@ -1,15 +1,8 @@
 from utils.sleep import *
 import logging
 import uuid
-import shutil
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.action_chains import ActionChains
-import random
-import time
+import os
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
